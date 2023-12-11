7e7e125899e51f1e52579ae59a1cd7ccdf97a817
henry-jacq <henryeditz267@gmail.com>
2023-12-11 23:46:37 +0530

    feat: Changing price of items in cart
    Changing the price of the items by quantity

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+// Add to car
 $('.btn-add-cart').on('click', function(e) {
     product_id = $(this).attr('data-id');
 
@@ -7,44 +8,69 @@ $('.btn-add-cart').on('click', function(e) {
     };
 
     if ($(this).children().hasClass('bi-cart-plus')) {
-        $(this).html('');
-        $(this).html('<i class="bi bi-check2 me-2"></i>Added to cart');
-        if ($(this).hasClass('btn-outline-secondary')) {
-            $(this).removeClass('btn-outline-secondary');
-            $(this).addClass('btn-secondary')
-        }
-        $(this).prev().attr('readonly', 'true');
         $.ajax({
             url: "/api/cart/add",
             type: "POST",
             contentType: "application/json", // Set the Content-Type header
             data: JSON.stringify(data),
-            success: function(response) {
-                console.log(response);
+            success: function() {
+                $(this).html('');
+                $(this).html('<i class="bi bi-check2 me-2"></i>Added to cart');
+                if ($(this).hasClass('btn-outline-secondary')) {
+                    $(this).removeClass('btn-outline-secondary');
+                    $(this).addClass('btn-secondary')
+                }
+                $(this).prev().attr('readonly', 'true');
             },
             error: function(error) {
-                console.log(error);
+                console.error(error);
             }
         });
     } else {
-        $(this).html('');
-        $(this).html('<i class="bi bi-cart-plus me-2"></i>Add to cart');
-        if ($(this).hasClass('btn-secondary')) {
-            $(this).removeClass('btn-secondary');
-            $(this).addClass('btn-outline-secondary')
-        }
-        $(this).prev().removeAttr('readonly')
         $.ajax({
             url: "/api/cart/remove",
             type: "POST",
             contentType: "application/json", // Set the Content-Type header
             data: JSON.stringify(data),
-            success: function(response) {
-                console.log(response);
+            success: function() {
+                $(this).html('');
+                $(this).html('<i class="bi bi-cart-plus me-2"></i>Add to cart');
+                if ($(this).hasClass('btn-secondary')) {
+                    $(this).removeClass('btn-secondary');
+                    $(this).addClass('btn-outline-secondary')
+                }
+                $(this).prev().removeAttr('readonly')
             },
             error: function(error) {
-                console.log(error);
+                console.error(error);
             }
         });
     }
+});
+
+// Increase count of items
+$('.items-increase').click(function(e) {
+    c_qty = parseInt($(this).prev().html());
+    max = parseInt($(this).prev().attr('data-max'))
+    total_price_el = $(this).parents('.quantity-section').next().find('.total-item-price');
+    original_price = parseInt($(this).parents('.quantity-section').prev().find('.price-per-item').html())
+    if (max > c_qty) {
+        c_qty += 1;
+        new_price = original_price + parseInt(total_price_el.html())
+        total_price_el.html(new_price)
+    }
+    $(this).prev().html(c_qty);
+});
+
+// Decrease count of items
+$('.items-decrease').click(function(e) {
+    c_qty = parseInt($(this).next().html());
+    total_price_el = $(this).parents('.quantity-section').next().find('.total-item-price');
+    original_price = parseInt($(this).parents('.quantity-section').prev().find('.price-per-item').html())
+    if (c_qty > 1) {
+        c_qty -= 1;
+        new_price = parseInt(total_price_el.html()) - original_price
+        total_price_el.html(new_price)
+    }
+    $(this).next().html(c_qty);
 });
