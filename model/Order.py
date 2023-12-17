3484a69d009eec6a7bf16d82574e822595abc018
henry-jacq <henryeditz267@gmail.com>
2023-12-17 10:53:07 +0530

    Minor changes

--- model/Order.py
+++ model/Order.py
@@ -7,7 +7,7 @@ class Order:
         self.table2 = 'order_items'
         
     def create_order(self, customer_id, order_date, status, amount):
-        if self.cart_exists(customer_id) != True:
+        if self.order_exists(customer_id) != True:
             result = self.db.insert(self.table1, [customer_id, order_date, status, amount], sequence="ORDER_SEQ.NEXTVAL")
             return result
         return False
@@ -18,15 +18,15 @@ class Order:
             return True
         return False
 
-    def get_order_id(self, customer_id):
-        result = self.db.select(self.table1, ['cart_id'], f"customer_id = {customer_id}")
+    def get_order_id(self, customer_id, fetchModeAll=False):
+        result = self.db.select(self.table1, ['order_id'], f"customer_id = {customer_id}", fetchAll=fetchModeAll)
         if result is not None:
             return result[0]
         return False
     
     # Add item to order items
     def add_item(self, order_id, product_id, quantity, unit_price):
-        if self.item_exists(order_id, product_id) == False:
-            self.db.insert(self.table2, [order_id, product_id, quantity, unit_price], sequence="ORDER_ITEM_SEQ.NEXTVAL")
+        result = self.db.insert(self.table2, [order_id, product_id, quantity, unit_price], sequence="ORDER_ITEM_SEQ.NEXTVAL")
+        if result:
             return True
         return False
