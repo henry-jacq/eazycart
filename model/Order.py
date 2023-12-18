0070d6fc1e5309ce0b23e3c82c90dc0c7743b43d
henry-jacq <henryeditz267@gmail.com>
2023-12-19 00:08:48 +0530

    change: create order updated

--- model/Order.py
+++ model/Order.py
@@ -1,4 +1,5 @@
 from model.Database import Database
+from datetime import datetime
 
 class Order:
     def __init__(self):
@@ -6,11 +7,12 @@ class Order:
         self.table1 = 'orders'
         self.table2 = 'order_items'
         
-    def create_order(self, customer_id, order_date, status, amount):
-        if self.order_exists(customer_id) != True:
-            result = self.db.insert(self.table1, [customer_id, order_date, status, amount], sequence="ORDER_SEQ.NEXTVAL")
-            return result
-        return False
+    def create_order(self, customer_id, status, amount):
+        date = datetime.now().strftime('%Y-%m-%d')
+        sql = f"INSERT INTO orders VALUES (ORDER_SEQ.NEXTVAL, {customer_id}, TO_DATE('{date}', 'YYYY-MM-DD'), '{status}', {amount})"
+        query = self.db.exec_raw(sql)
+        self.db.commit()      
+        return query
 
     def order_exists(self, customer_id):
         result = self.db.select(self.table1, "*", f"customer_id = {customer_id}")
